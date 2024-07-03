78fb7d8c7fd309786cf7e108a64dd6d120ddb9b9
root <sai.marthala0799@example.com>
2024-07-03 14:28:28 +0000

    my first commit

---
+++ basicmd.sh
@@ -0,0 +1,38 @@
+Basics linux and aws cmd's
+=================================
+Change hostname:
+whoami
+sudo -i 
+whoami
+hostnamectl set-hostname aws-cli
+bash
+hostname
+===============
+Check OS version & Update Ubuntu packages:
+cat /etc/os-release
+sudo apt-get update
+===========
+
+How  to  Install aws cli---
+
+we have in  home dir
+2. then  we have to  use the cmd curl  ......
+3. update the curl :  sudo apt-get install curl && sudo apt-get install unzip
+4. then run the curl zip file cmd frim aws doc
+5. unzip the awscliv2.zip file
+6. ls -lART , and the go to the aws , use sudo ./install
+
+it will get installted in your systenm
+
+
+===========================
+install git in ubuntu
+
+
+apt-get install git
+-- once installed  in lunix give git soo that we  get know whether it is installed or not
+-- then give == git  init  which initalize the git repo in lunix
+
+---- git config== git config --global user.email "your_email@example.com"
+
+
